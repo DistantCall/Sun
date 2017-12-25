dba76fe45c4e946dfa0a248ba9b837593026a331
DistantCall <3238306450@qq.com>
2017-12-25 14:23:54 +0800

    first commit

---
+++ app/src/main/java/com/example/dell/likeshift/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.dell.likeshift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String 我是大傻鹅;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
